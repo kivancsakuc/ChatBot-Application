1a8f7b95beb9a7a6696e495df9e768163d5e24f7
Atahan3 <168367590+Atahan3@users.noreply.github.com>
2025-05-10 17:30:06 +0300

    Revert "roomdatabase temelleri atıldı"
    This reverts commit dea4b9c825d8a0f90d63d3d8ef15141f2c2175e0.

--- app/src/main/java/com/example/chatbotapplication/ChatMessage.java
+++ app/src/main/java/com/example/chatbotapplication/ChatMessage.java
@@ -1,25 +1,15 @@
 package com.example.chatbotapplication;
 
-import androidx.room.Entity;
-import androidx.room.PrimaryKey;
-
-@Entity
 public class ChatMessage {
-
-    @PrimaryKey(autoGenerate = true)
-    public int id;
-
     public static final int TYPE_USER = 0;
     public static final int TYPE_BOT = 1;
 
     private String message;
     private int type;
-    private int sessionId;
 
-    public ChatMessage(String message, int type, int sessionId) {
+    public ChatMessage(String message, int type) {
         this.message = message;
         this.type = type;
-        this.sessionId = sessionId;
     }
 
     public String getMessage() {
@@ -28,9 +18,5 @@ public class ChatMessage {
 
     public int getType() {
         return type;
-    }
-
-    public int getSessionId() {
-        return sessionId;
     }
 }
